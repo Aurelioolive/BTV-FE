9972a8c1eea185c80f8abedc91c0d9307651776b
Aurélio Henrique <aurelio.oliveira@apinteractive.com.br>
2020-08-23 02:41:02 -0300

    Created routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './pages/admin/admin.component';
+import { IndexComponent } from './pages/index/index.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: 'admin', component: AdminComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './pages/index/index.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
